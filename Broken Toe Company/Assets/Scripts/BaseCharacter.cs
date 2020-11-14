a12abecec4e57d50ea9d9645757619f8b67d8502
Antoine <antoine35.colin@gmail.com>
2020-11-14 12:56:46 +0100

    Début création UI personnage

--- Broken Toe Company/Assets/Scripts/BaseCharacter.cs
+++ Broken Toe Company/Assets/Scripts/BaseCharacter.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class BaseCharacter : MonoBehaviour
 {
-    string surname;
-    int strength;
-    int speed;
-    int mind;
+    public string surname;
+    [Range(-3, 3)]
+    public int strength;
+    [Range(-3, 3)]
+    public int speed;
+    [Range(-3, 3)]
+    public int mind;
 
     public enum Skill { VILLAGER, FISHER, HUNTER, WARRIOR, MAGE, NECROMANCER, COOK, PALADIN, DEMONIST, ARTIFICER, GATHERER, TRESORHUNTER, BARBARIAN, SPRINTER, SAGE, PRINCESS, THIEF, STRATEGIST, JUDGE, TANK, SCOUT}
     public Skill currentSkill = Skill.VILLAGER;
 
     void Start()
     {
-        
+        System.Random rand = new System.Random();
+        int choice = rand.Next(typeof(Skill).GetMembers().Length - 1);
+
     }
 
     void JoinCompany()
