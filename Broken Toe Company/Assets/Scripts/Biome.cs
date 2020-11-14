6f8f10eff2216d7d3d2380a4cdb5288efb2992bf
Simon Binet <binetsimon@gmail.com>
2020-11-14 12:09:07 +0100

    biome

--- Broken Toe Company/Assets/Scripts/Biome.cs
+++ Broken Toe Company/Assets/Scripts/Biome.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class Biome : MonoBehaviour
 {
+    public enum TypeBiome { CLAIRIERE, CHAMP, FORET, VILLAGE, CHATEAU, CRYPTE, FORET_NOIRE, CIMETIERE }
+    public TypeBiome currentBiome = TypeBiome.CLAIRIERE;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,12 @@ public class Biome : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void pickBiome()
+    {
+        Array values = Enum.GetValues(typeof(TypeBiome));
+        Random random = new Random();
+        currentBiome = (TypeBiome)values.GetValue(random.Next(values.Length));
     }
 }
