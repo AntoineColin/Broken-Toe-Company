18eb9d03b1a978bd9296437e124e98f75f687103
Simon Binet <binetsimon@gmail.com>
2020-11-14 22:53:20 +0100

    timer

--- Broken Toe Company/Assets/Scripts/GameManager.cs
+++ Broken Toe Company/Assets/Scripts/GameManager.cs
@@ -6,14 +6,34 @@ public class GameManager : MonoBehaviour
 {
     
     public BaseEncounter currentEncounter;
+    public float chrono = 10;
+    public TimerChoose timerChoose;
 
     void Start()
     {
         
     }
 
+    private void Update()
+    {
+        chronoUpdate();
+        if (chrono == 0)
+        {
+            //Biome.changeEncounter();
+            chrono = 5;
+            timerChoose.ResetSlider(chrono);
+        }
+            
+    }
+
     void GenerateChoices(int choiceNumber)
     {
 
+    }
+
+    void chronoUpdate()
+    {
+        chrono -= Time.deltaTime;
+        timerChoose.setSliderVal(chrono);
     }
 }
---
+++ Broken Toe Company/Assets/Scripts/TimerChoose.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerChoose : MonoBehaviour
+{
+    public Slider slider;
+
+    public void ResetSlider(float maxval)
+    {
+        slider.maxValue = maxval;
+    }
+
+    public void setSliderVal(float val)
+    {
+        slider.value = val;
+    }
+
+}
